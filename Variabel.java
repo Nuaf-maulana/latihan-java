f1d090dddddcfa84d282eedaef1721afa96b3aaf
Nuaf-maulana <110182614+Nuaf-maulana@users.noreply.github.com>
2022-08-29 20:04:09 +0700

    membuat variabel
    setelah membuat hello word saya membuat variabel dari bahasa pemrograman java

---
+++ Variabel.java
@@ -0,0 +1,17 @@
+// belajar menulis program java berbentuk variabel
+class Variabel{
+    public static void main(String[] args) {
+        System.out.println("hello pak guru");
+        System.out.printf("wirosablem, naga geni %d\n", 212);
+        //menampilkan nilai assignmant
+    int a = 10;
+    System.out.println("nilai a =" +a);
+    // variabel bisa berubah-ubah
+    a = 20;
+    System.out.println("nilai a baru =" +a);
+    // membuat deklarasi
+    int b;//deklarasi
+    b = 7;
+    System.out.println("nilai b =" +b);
+    }
+}
